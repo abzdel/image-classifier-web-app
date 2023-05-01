7dcb6a1bbc8d03873a822cf29afe8e47490bd13e
Alex Bzdel <ab913@duke.edu>
2023-05-01 16:49:47 +0000

    added backend + starting to implement frontend

---
+++ Makefile
@@ -0,0 +1,9 @@
+install-aws:
+	curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+	sudo mv /tmp/eksctl /usr/local/bin
+
+	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	unzip awscliv2.zip
+install:	
+	sudo ./aws/install
+	pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+#from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
+import numpy as np
+from transformers import AutoFeatureExtractor, ResNetForImageClassification
+
+
+app = Flask(__name__)
+
+# Load the pre-trained model from Hugging Face
+model_name = "microsoft/resnet-18" # Replace with the name of your pre-trained model
+#tokenizer = AutoTokenizer.from_pretrained(model_name)
+#model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-18")
+model = ResNetForImageClassification.from_pretrained("microsoft/resnet-18")
+
+model.eval()
+
+# Define a Flask API route to accept images
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the image from the request
+    image_file = request.files['image']
+    image = Image.open(image_file)
+
+    # Process the image to match the input format of the model
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+    image_tensor = transform(image).unsqueeze(0)
+
+    # Make a prediction using the pre-trained model
+    with torch.no_grad():
+        outputs = model(image_tensor)
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=1)
+        predicted_class = torch.argmax(probabilities, dim=1).item()
+
+    # Return the prediction to the user
+    return jsonify({'prediction': predicted_class})
+
+# Define a Flask API route to serve the HTML file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

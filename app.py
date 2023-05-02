8c7c088d48595875d698572d1deccb9f665062bd
Alex Bzdel <ab913@duke.edu>
2023-05-02 02:33:40 +0000

    renamed files

--- app.py
+++ app.py
@@ -1,55 +1,45 @@
-from flask import Flask, request, jsonify, render_template
-import torch
-from PIL import Image
-import torchvision.transforms as transforms
-import numpy as np
-from transformers import AutoFeatureExtractor, ResNetForImageClassification
+import boto3
+from flask import Flask, jsonify, render_template, request
+import base64
 import os
+import json, requests
+import io
+from PIL import Image
+
 
 app = Flask(__name__)
 
-# Load the pre-trained model from Hugging Face
-model_name = "microsoft/resnet-18" # Replace with the name of your pre-trained model
-#tokenizer = AutoTokenizer.from_pretrained(model_name)
-#model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-18")
-model = ResNetForImageClassification.from_pretrained("microsoft/resnet-18")
-
-model.eval()
-
-# Define a Flask API route to accept images
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict():
-    print('Received request for prediction')
+    if request.method == 'POST':
 
-    # show the image that the user has sent
-    print(request.files['image'])
+        print("Classifying image...")
+        # Read the image file from the request
+        image_file = request.files['image']
+        image_data = image_file.read()
+        
+        # Encode the image data in base64 format
+        encoded_image = base64.b64encode(image_data).decode('utf-8')
 
-    # Get the image from the request
-    image_file = request.files['image']
-    image = Image.open(image_file)
+        # Create the payload for the Lambda function
+        event = {
+            'body': encoded_image
+        }
 
+        # Invoke the Lambda function
+        client = boto3.client('lambda')
+        response = client.invoke(
+            FunctionName='classify',
+            Payload=json.dumps(event)
+        )
 
-    image = image.resize((224, 224))
-    inputs = feature_extractor(image, return_tensors="pt")
+        # Parse the response from the Lambda function
+        response_dict = json.loads(response['Payload'].read().decode('utf-8'))
+        #predictions = response_dict.get('predictions')
 
+        return json.loads(response_dict.get('body'))
 
-
-    with torch.no_grad():
-        logits = model(**inputs).logits
-
-    # model predicts one of the 1000 ImageNet classes
-    predicted_label = logits.argmax(-1).item()
-    print(f'Predicted class: {model.config.id2label[predicted_label]}')
-
-    # Return the prediction to the user
-    #return jsonify({'prediction': model.config.id2label[predicted_label]})
-    return model.config.id2label[predicted_label]
-
-# Define a Flask API route to serve the HTML file
-@app.route('/')
-def index():
+    # render the home template for GET requests
     return render_template('index.html')
 
 if __name__ == '__main__':
---
+++ local_app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, render_template
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
+import numpy as np
+from transformers import AutoFeatureExtractor, ResNetForImageClassification
+import os
+
+app = Flask(__name__)
+
+# Load the pre-trained model from Hugging Face
+model_name = "microsoft/resnet-18" # Replace with the name of your pre-trained model
+#tokenizer = AutoTokenizer.from_pretrained(model_name)
+#model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-18")
+model = ResNetForImageClassification.from_pretrained("microsoft/resnet-18")
+
+model.eval()
+
+# Define a Flask API route to accept images
+@app.route('/predict', methods=['POST'])
+def predict():
+    print('Received request for prediction')
+
+    # show the image that the user has sent
+    print(request.files['image'])
+
+    # Get the image from the request
+    image_file = request.files['image']
+    image = Image.open(image_file)
+
+
+    image = image.resize((224, 224))
+    inputs = feature_extractor(image, return_tensors="pt")
+
+
+
+    with torch.no_grad():
+        logits = model(**inputs).logits
+
+    # model predicts one of the 1000 ImageNet classes
+    predicted_label = logits.argmax(-1).item()
+    print(f'Predicted class: {model.config.id2label[predicted_label]}')
+
+    # Return the prediction to the user
+    #return jsonify({'prediction': model.config.id2label[predicted_label]})
+    return model.config.id2label[predicted_label]
+
+# Define a Flask API route to serve the HTML file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

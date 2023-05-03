7f5fd1cd7199def9968e1e325f6c9d1fa4b6c0bb
Alex Bzdel <ab913@duke.edu>
2023-05-03 21:14:13 +0000

    fixed req

--- loadtest.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, between, task
-
-class WebsiteUser(HttpUser):
-    wait_time = between(5, 9)
-
-    @task
-    def predict(self):
-        self.client.post("/predict", files={"image": open("tiger.jpg", "rb")})
